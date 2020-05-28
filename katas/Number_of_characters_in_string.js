e107e92069c397ab395d9d39e2f6775f36c8be85
Mathias Gautier <mths.gautier@gmail.com>
2020-05-28 16:10:39 +0200

    counting letters as object

---
+++ katas/Number_of_characters_in_string.js
@@ -0,0 +1,20 @@
+// The main idea is to count all the occurring characters(UTF-8) in string. If you have string like this aba then the result should be { 'a': 2, 'b': 1 }
+
+// What if the string is empty ? Then the result should be empty object literal { }
+
+let str = "oollaala";
+
+function res(string) {
+    let char = string.split('');
+    let count = {}
+    for (let i = 0; i < char.length; i++)
+        !count[char[i]] ? count[char[i]] = 1 :
+        count[char[i]] += 1
+    return count
+}
+
+
+
+
+
+console.log(res(str))

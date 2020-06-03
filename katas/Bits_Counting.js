f330616bdae82a9624f72abc44039a97ec75f0eb
Mathias Gautier <mths.gautier@gmail.com>
2020-06-03 12:07:19 +0200

    bits counting

---
+++ katas/Bits_Counting.js
@@ -0,0 +1,14 @@
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+var countBits = function (n) {
+    return (n >>> 0)
+        .toString(2)
+        .split('')
+        .filter(i => i == 1)
+        .length
+};
+
+
+console.log(countBits(1234))

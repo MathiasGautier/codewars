7ceb2e9a624ba8dfcda5b75f337aefe90b525f2c
Mathias Gautier <mths.gautier@gmail.com>
2020-09-05 16:41:11 +0200

    new method for "Find the missing letter"

--- katas/Find_the_missing_letter.js
+++ katas/Find_the_missing_letter.js
@@ -18,10 +18,32 @@
 // I have also created other katas. Take a look if you enjoyed this kata!
 
 function findMissingLetter(array) {
-    let str2arr = array.join("");
-    for (let i = 0; i < str2arr.length; i++) {
-      if (str2arr.charCodeAt(i + 1) - str2arr.charCodeAt(i) != 1) {
-        return String.fromCharCode(str2arr.charCodeAt(i) + 1);
-      }
+  let str2arr = array.join("");
+  for (let i = 0; i < str2arr.length; i++) {
+    if (str2arr.charCodeAt(i + 1) - str2arr.charCodeAt(i) != 1) {
+      return String.fromCharCode(str2arr.charCodeAt(i) + 1);
     }
   }
+}
+
+
+const newfunction = ((array) => {
+  let alph = "abcdefghijklmnopqrstuvwxyz".split('');
+  //check if the array is in upper or lower case and transform the alphabet if needed
+  if (array.toString() === array.toString().toUpperCase()) {
+    alph = alph.toString().toUpperCase().split(',')
+  }
+  //find starting and ending points to build a new array
+  let start = alph.indexOf(alph.filter((x, i) => x === array[0]).join(''));
+  let end = alph.indexOf(alph.filter((x, i) => x === array[array.length - 1]).join(''));
+
+  let newAlphArr = [];
+  for (i = start; i < end + 1; i++) {
+    newAlphArr.push(alph[i])
+  }
+  //compare the two arrays to fin the missing letter
+  return newAlphArr.filter((letter) => !array.includes(letter)).toString();
+
+})
+
+console.log(newfunction(["A", "C", "D", "E"]))
